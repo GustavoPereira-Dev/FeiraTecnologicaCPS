d50aa2ad93479111e7e1ec96d5c1c41d8c04868d
Gustavo-Henrique-da-Silva <gustavohenrique10002@gmail.com>
2022-11-17 12:35:48 -0300

    Corrigindo o erro do pwa no Github

--- pwa/main.js
+++ pwa/main.js
@@ -1,6 +1,10 @@
 if ('serviceWorker' in navigator) {
     console.log("main.js => Vamos registrar o service worker!!!");
+<<<<<<< HEAD
     navigator.serviceWorker.register('/service-worker-minimum-to-intall-pwa.js')
+=======
+    navigator.serviceWorker.register('/FeiraTecnologicaCPS/service-worker-minimum-to-intall-pwa.js')
+>>>>>>> 6609566 (Corrigindo o erro do pwa no Github)
         .then(registration => {
             console.log("main.js => Service Worker Registrado com Sucesso");
             console.dir(registration);
