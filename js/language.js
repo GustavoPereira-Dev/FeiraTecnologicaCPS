643e6d351584d5fa5e8b67e8dae56818129a3c0b
Gustavo Henrique <gustavohenrique10002@gmail.com>
2022-11-24 22:42:06 -0300

    Adicionado mais coisas ao Formulário do Site e funcionalidades

--- js/language.js
+++ js/language.js
@@ -8,3 +8,14 @@ document.addEventListener('DOMContentLoaded', function() {
    $(document).ready(function(){
     $('.materialboxed').materialbox();
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
